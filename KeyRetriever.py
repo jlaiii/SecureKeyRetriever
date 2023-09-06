eeafea481c2bba779f53782f2bed37519fac4a97
Jay <20913004+jlaiii@users.noreply.github.com>
2023-09-06 17:35:50 -0500

    Add files via upload

--- KeyRetriever.py
+++ KeyRetriever.py
@@ -7,7 +7,7 @@ import random
 import string
 
 UPLOAD_DIRECTORY = "keypages"
-LINK_LENGTH = 8
+LINK_LENGTH = random.randint(2, 4)  # Random length between 5 and 10
 GENERATED_KEYS_FILE = "generated_keys.txt"
 
 def generate_random_link(length):
@@ -17,7 +17,7 @@ def generate_random_link(length):
 def get_and_remove_key():
     with open(GENERATED_KEYS_FILE, "r") as file:
         keys = file.readlines()
-    
+
     if keys:
         key = keys[0].strip()  # Get the first key and remove leading/trailing whitespaces
         with open(GENERATED_KEYS_FILE, "w") as file:
@@ -45,8 +45,8 @@ class KeyServerHandler(BaseHTTPRequestHandler):
             response += '</form>'
             response += "</body></html>"
             self.wfile.write(response.encode())
-        elif self.path.startswith("/uploads/"):
-            # Serve the generated HTML pages from the uploads directory
+        elif self.path.startswith("/keys/"):
+            # Serve the generated HTML pages from the keys directory
             page_path = os.path.join(UPLOAD_DIRECTORY, os.path.basename(self.path))
             if os.path.exists(page_path):
                 self._set_response()
@@ -64,21 +64,24 @@ class KeyServerHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         if self.path == "/get_key":
             self._set_response()
-            
+
             # Get and remove a key from the list
             key = get_and_remove_key()
-            
+
             if key:
                 # Generate a unique HTML page filename
-                page_link = generate_random_link(LINK_LENGTH)
-                page_filename = os.path.join(UPLOAD_DIRECTORY, page_link + ".html")
-                
+                while True:
+                    page_link = generate_random_link(LINK_LENGTH)
+                    page_filename = os.path.join(UPLOAD_DIRECTORY, page_link + ".html")
+                    if not os.path.exists(page_filename):
+                        break
+
                 # Create the HTML page with the retrieved key
                 with open(page_filename, 'w') as html_file:
                     html_file.write(f"<html><body><h1>Your Key</h1><p>{key}</p>")
                     html_file.write('<a href="/">Home</a>')  # Add the Home button
                     html_file.write("</body></html>")
-                
+
                 # Send the response with JavaScript for the delay
                 response = """
                 <html>
@@ -86,7 +89,7 @@ class KeyServerHandler(BaseHTTPRequestHandler):
                 <title>Redirecting...</title>
                 <script>
                 setTimeout(function() {
-                    window.location.href = '/uploads/""" + page_link + """.html';
+                    window.location.href = '/keys/""" + page_link + """.html';
                 }, 2000); // 2-second delay
                 </script>
                 </head>
